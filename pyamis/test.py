0276d1819eefef586a6e68f45531dadc85170344
kylin <260987762@qq.com>
2024-09-18 11:45:56 +0800

    提交

---
+++ pyamis/test.py
@@ -0,0 +1,8 @@
+from amis.layout.page import Page
+from amis.function.app import App
+
+
+page = Page()
+app = App()
+print(page.to_dict())
+print(app.to_dict())
---
+++ src-nuxt/components/amis/AmisRender.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import {  onMounted } from "vue";
+import 'amis/sdk/sdk.js';
+import 'amis/lib/themes/default.css';
+const props= defineProps({
+  amisjson: {
+    type: Object,
+    required: true
+  }
+})
+
+
+onMounted(() => {
+  // @ts-ignore
+  const amis = amisRequire('amis/embed');
+  amis.embed('#box', props.amisjson);
+})
+
+</script>
+<template>
+  <div id="box"></div>
+</template>
+<style>
+</style>
+
